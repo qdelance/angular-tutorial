dfc7419fae9f41ac59f78c03ce627448d936c79e
Quentin Delance <quentin.delance@gmail.com>
2016-09-21 23:02:34 +0200

    Half of step #6 (routing)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+
 import { AppComponent }   from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }   from './heroes.component';
 import { HeroDetailComponent }   from './hero-detail.component';
 
+import { HeroService } from './hero.service';
+import { Logger } from './logger.service';
+
+import { routing } from './app.routing';
+
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HeroDetailComponent],
-    bootstrap: [AppComponent]
+    imports: [BrowserModule, FormsModule, routing],
+    declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent],
+    bootstrap: [AppComponent],
+    providers: [HeroService, Logger]
 })
 export class AppModule {
 }
---
+++ app/app.routing.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent }      from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
