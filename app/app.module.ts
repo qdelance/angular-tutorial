50e572994f1b16b9083be1c6472213c00f4a1dac
Quentin Delance <quentin.delance@gmail.com>
2016-09-25 10:25:54 +0200

    Half of step #7 (mock http API)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }   from './app.component';
 import { DashboardComponent }   from './dashboard.component';
@@ -13,8 +18,19 @@ import { Logger } from './logger.service';
 import { routing } from './app.routing';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, routing],
-    declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        HeroesComponent,
+        HeroDetailComponent
+    ],
     bootstrap: [AppComponent],
     providers: [HeroService, Logger]
 })
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,23 +1,54 @@
 import { Injectable } from '@angular/core';
+
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Logger } from './logger.service';
 
 @Injectable()
 export class HeroService {
 
-    constructor(private logger: Logger) {
+    private heroesUrl = 'app/heroes';  // URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private logger: Logger,
+                private http: Http) {
+        this.logger.log('HeroService: constructor');
+    }
+
+    create(name: string): Promise<Hero> {
+        this.logger.log('Creating new hero with name "' + name + '"');
+        // TODO, with a more complex object, I would probably create a new instance here then stringify it below
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        this.logger.log('Updating hero for id ' + hero.id);
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
     }
 
     getHero(id: number): Promise<Hero> {
-        this.logger.log('Getting hero for id ' + id + ' ...');
+        this.logger.log('Getting hero for id ' + id);
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
     }
 
     getHeroes(): Promise<Hero[]> {
-        this.logger.log('Getting heroes ...');
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
 
     // to simulate slow connection: https://angular.io/docs/ts/latest/tutorial/toh-pt4.html#!#slow
@@ -25,5 +56,10 @@ export class HeroService {
         return new Promise<Hero[]>(resolve =>
             setTimeout(resolve, 2000)) // delay 2 seconds
             .then(() => this.getHeroes());
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
